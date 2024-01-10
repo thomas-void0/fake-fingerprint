eb46e36938fbfa58ee8e0d7c11945341c35ff8a8
thomas-void0 <yjy15680489038@163.com>
2024-01-10 09:22:13 +0700

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export const tsup = defineConfig((option) => ({
+  entry: ['src/index.ts'],
+  target: 'node16',
+  dts: true,
+  clean: true,
+  format: ['cjs', 'esm'],
+  platform: 'node',
+  splitting: false,
+  treeshake: true,
+  minify: false,
+  sourcemap: !!option.watch,
+  tsconfig: option.watch ? 'tsconfig.dev.json' : 'tsconfig.json',
+}))
